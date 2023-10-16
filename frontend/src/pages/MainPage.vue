fd3b9389607ea8dcad4f77f1387a8d29a7e4dcd3
lapvv <lapvv@yandex.ru>
2023-10-16 11:12:27 +0000

    wip

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>
-        MainPage
-    </div>
+  <div>MainPage</div>
+  <button class="btn btn-primary" @click="sendMoves"></button>
 </template>
 
 <script>
-    export default {
-        
+const websocket = new WebSocket("ws://localhost:8001/");
+const event = { type: "play", column: 3 };
+websocket.send(JSON.stringify(event));
+function sendMoves(board, websocket) {
+  // When clicking a column, send a "play" event for a move in that column.
+  board.addEventListener("click", ({ target }) => {
+    const column = target.dataset.column;
+    // Ignore clicks outside a column.
+    if (column === undefined) {
+      return;
     }
+    const event = {
+      type: "play",
+      column: parseInt(column, 10),
+    };
+    websocket.send(JSON.stringify(event));
+  });
+}
+sendMoves();
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
